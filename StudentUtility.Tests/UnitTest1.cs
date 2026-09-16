f75b1b19bf8ce26a0cd5efa66f1402fe12cfe2f9
Sagar_Sharma <contact.sagarsharma19@gmail.com>
2026-09-16 13:36:08 +0530

    Initial commit - Jenkins CI/CD demo

---
+++ StudentUtility.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentUtility.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void AddNumbers_ReturnsCorrectResult()
+    {
+        var result = StudentHelper.AddNumbers(2, 3);
+
+        Assert.Equal(5, result);
+    }
+}
---
+++ StudentUtility/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentUtility;
+
+public class StudentHelper
+{
+    public static int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+}
